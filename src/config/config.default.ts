4f62ec0e0a48d7da368dbe589ffdf51dbf6d3adf
henryliu <17600047539@163.com>
2022-05-18 19:50:57 +0800

    add local

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -4,18 +4,18 @@ export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1648288882537_103',
   koa: {
-    port: 3004,
+    port: 7001,
   },
   orm: {
     /**
      * 单数据库实例
      */
     type: 'postgres',
-    host: 'pgm-3ns8ck0510ci7s311o.pg.rds.aliyuncs.com',
+    host: '',
     port: 5432,
-    username: 'henry',
-    password: 'Liuhongqi321',
-    database: "devnet-faucet",
+    username: 'postgres',
+    password: '123456',
+    database: undefined,
     synchronize: true, // 如果第一次使用，不存在表，有同步的需求可以写 true
     logging: true,
   },
